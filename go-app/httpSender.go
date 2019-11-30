f2bc76b8a8c9131e40ba44d44f3a3290d109c8ed
Cameron Massey <cameron_massey@yahoo.com>
2019-11-30 11:33:35 -0800

    Cleaned up Golang code and fixed string building issue
    Cleaned up and improved readability for golang code.
    
    I also fixed an issue with building the http request url's. Previously they were not being reset for each data object, and the same string was being set for each url.

--- go-app/httpSender.go
+++ go-app/httpSender.go
@@ -6,6 +6,7 @@ import (
 		"encoding/json"
 		"time"
 		"strings"
+		"errors"
 		"net/http"
 		"io/ioutil"
 		"github.com/tcnksm/go-httpstat"
@@ -18,69 +19,57 @@ type Postback struct {
 	Data []map[string]string
 }
 
-func sendHttpRequest(pback Postback){
+func buildHttpUrls(pback Postback) []string {
 
-	switch pback.RequestMethod {
-	case "GET":
-			var httpUrl string = pback.Url
-			for _, dataMap := range pback.Data {
-				for k, v := range dataMap {
-					//fmt.Println("k:", k, "v:", v)
+	requestUrls := make([]string,len(pback.Data))
 
-					httpUrl = strings.Replace(httpUrl, "{" + k + "}", v, 1)
-					//fmt.Println("Request String: " + httpUrl)
-				}
-
-				//send http request
-				req, err := http.NewRequest("GET", httpUrl, nil)
-				if err != nil {
-					fmt.Println(err)
-					log.Fatal(err)
-				}
-				//log delivery time, respose code, response time
-				//and response body
-
-				var result httpstat.Result
-				ctx := httpstat.WithHTTPStat(req.Context(), &result)
-				req = req.WithContext(ctx)
-
-				client := http.DefaultClient
-				res, err := client.Do(req)
-				if err != nil {
-				    log.Fatal(err)
-				}
-
-				if res.StatusCode == http.StatusOK {
-			    bodyBytes, err := ioutil.ReadAll(res.Body)
-			    if err != nil {
-			        log.Fatal(err)
-			    }
-			    bodyString := string(bodyBytes)
-			    log.Printf(bodyString)
-
-				}
-
-
-				res.Body.Close()
-				result.End(time.Now())
-				log.Printf(result.Total(time.Now()).String())
-				log.Printf(res.Status)
-				//fmt.Println(result.Total(time.Now()))
-
-				// Show results
-				//log.Printf("%+v", result)
-
-				fmt.Println(req)
-			}
-
-		case "POST":
-			//fmt.Println("POST")
-		default:
-			//fmt.Println("ERROR")
+	for index, dataMap := range pback.Data {
+		var httpUrl string = pback.Url
+		for k, v := range dataMap {
+			r := strings.NewReplacer("{" + k + "}", v)
+			httpUrl = r.Replace(httpUrl)
+		}
+		requestUrls[index] = httpUrl
 	}
 
-	//resp, err := http.PostForm("http://example.com/form",
-	//url.Values{"key": {"Value"}, "id": {"123"}})
+	return requestUrls
+}
+
+func sendHttpRequest(httpUrl string, requestMethod string) {
+
+	if requestMethod != "POST" && requestMethod != "GET" {
+		log.Fatal(errors.New("Bad request method: " + requestMethod))
+	}
+
+	req, err := http.NewRequest(requestMethod, httpUrl, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//log delivery time, respose code, response time, and response body
+	var result httpstat.Result
+	ctx := httpstat.WithHTTPStat(req.Context(), &result)
+	req = req.WithContext(ctx)
+
+	client := http.DefaultClient
+	res, err := client.Do(req)
+	if err != nil {
+			log.Fatal(err)
+	}
+
+	if res.StatusCode == http.StatusOK {
+		bodyBytes, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+				log.Fatal(err)
+		}
+		bodyString := string(bodyBytes)
+		log.Printf(bodyString)
+	}
+
+	res.Body.Close()
+	result.End(time.Now())
+
+	log.Printf(res.Status)
+	log.Printf("%+v", result)
 }
 
 func main() {
@@ -95,21 +84,26 @@ func main() {
 
 	for true {
 
-		val, err := redis.String(conn.Do("RPOP", "postback-list"))
+		redisVal, err := redis.String(conn.Do("RPOP", "postback-list"))
 		if err != nil {
 		    fmt.Println(err)
 		}
 
-		if len(val) > 0 {
+		if len(redisVal) > 0 {
 			fmt.Println("Grabbed request")
-			var valByte []byte = []byte(val)
+			var valByte []byte = []byte(redisVal)
 
 			var pback Postback
 			err = json.Unmarshal(valByte, &pback)
 
 			//Send http request
-			sendHttpRequest(pback)
-		} else{
+			var requestUrls []string = buildHttpUrls(pback)
+			//fmt.Println(requestUrls)
+
+			for _, url := range requestUrls {
+				sendHttpRequest(url, pback.RequestMethod)
+			}
+		}else {
 			fmt.Println("No requests left")
 			time.Sleep(1 * time.Second)
 		}
